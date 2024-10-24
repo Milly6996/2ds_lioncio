f926629a3a9c6f2a795ba8e673467cd662b8f4da
Milly6996 <kubica.santos.emily@gmail.com>
2024-10-24 14:55:17 +0000

    calculadora legal

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,25 +1,33 @@
 const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
 const btnCalcular = document.querySelector('#btnCalcular')
-const adicao = document.querySelector('#adicao')
 const resultado = document.querySelector('#resultado')
-const soma = document.querySelector('#soma')
-const subtracao = document.querySelector('#subtritacao')
-const multiplicacao = document.querySelector('#multiplicacao')
-const divisao = document.querySelector('#divisao')
+const soma = document.querySelector('.soma')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('.multiplicacao')
+const divisao = document.querySelector('.divisao')
 
 
 function calcular(){
     const n1 = Number (numero1.value)
     const n2 = Number (numero2.value)
-    if(typeof-n1 === 'number'
+    if (typeof n1 === 'number'
         && typeof n2 === 'number') {
-            const adicao = `A soma de ${n1} e ${n2} = ${n1+n2}`
-            alert (adicao)
+            const add = `A soma de ${n1} e  ${n2} = ${n1+n2}`
+            const sub = `A subtração de ${n1} e  ${n2} = ${n1-n2}`
+            const div = `A divisão de ${n1} e  ${n2} = ${n1/n2}`
+            const mult = `A multiplicação de ${n1} e  ${n2} = ${n1*n2}`
+
+            soma.innerHTML = add
+            subtracao.innerHTML = sub
+            divisao.innerHTML = div
+            multiplicacao.innerHTML = mult
+
         } else {
                 alert('Porfavor, digite um numero correto')
             }
         }
-btnCalcular.addEventListener('click', function(){
+btnCalcular.addEventListener('click', function(evento){
+    evento.preventDefault()
     calcular()
 })
